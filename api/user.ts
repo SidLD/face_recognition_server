5da17b2a7655503b48c380e5816e7fc3833449aa
cidlibril <cidlibril5@gmail.com>
2024-09-10 17:35:21 +0800

    hehe

--- api/user.ts
+++ api/user.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { verifyToken } from '../util/verify';
-import { attendanceLogin, getAttendanceLogin, getUsers, register, getUserAttendance } from '../controller/userController';
+import { attendanceLogin, getAttendanceLogin, getUsers, register, getUserAttendance, approveUser } from '../controller/userController';
 dotenv.config()
 const userAPI = express()
 
@@ -10,6 +10,7 @@ userAPI.get('/users', getUsers);
 userAPI.post('/attendance', attendanceLogin);
 userAPI.get('/attendance', getAttendanceLogin);
 userAPI.get('/attendance/user', getUserAttendance);
+userAPI.get('/user/status', approveUser);
 
 
 export default userAPI
